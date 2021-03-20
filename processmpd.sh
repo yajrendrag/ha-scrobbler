79e695c2f1dd8900f74658e6e68523b12f77ac38
Jay Gardner <jay@gardner.us.com>
2021-03-20 12:25:09 -0600

    updated instruction comments

--- processmpd.sh
+++ processmpd.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 MAILTO=""
-# if running outside of hass, you may wish to uncomment the /bin/echo line
-# below and edit the path to mpd-events.txt replacing YOUR_HASS_USER with your
+# if running outside of hass, uncomment the /bin/echo line below and
+# edit the path to mpd-events.txt replacing YOUR_HASS_USER with your
 # actual hass user name
 # /bin/echo "$@" >> /home/YOUR_HASS_USER/ha-scrobble/mpd-events.txt
 
